6d5e3155a12347a614f3998de69cf26ca01a76fb
Bautistagl <bautistagonzalezlazo@gmail.com>
2022-12-19 22:49:35 -0300

    actualizando origin

--- api/server.js
+++ api/server.js
@@ -22,7 +22,7 @@ app.use(
   cors({
     // Si aún no tenes deployado tu front en origin va la url local.
     // Una vez que se deploye el front acá va esa url que te entrega.
-    origin: "http://localhost:3000",
+    origin: "https://tmdb2.vercel.app/",
     methods: ["GET", "POST", "DELETE", "OPTIONS"],
     credentials: true,
   })
