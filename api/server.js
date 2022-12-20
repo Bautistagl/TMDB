b81bedb382a7629c013cd883896177fa6ff99fe0
Bautistagl <bautistagonzalezlazo@gmail.com>
2022-12-19 23:14:09 -0300

    cors

--- api/server.js
+++ api/server.js
@@ -20,9 +20,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    // Si aún no tenes deployado tu front en origin va la url local.
-    // Una vez que se deploye el front acá va esa url que te entrega.
-    origin: "https://tmdb2.vercel.app/",
     methods: ["GET", "POST", "DELETE", "OPTIONS"],
     credentials: true,
   })
